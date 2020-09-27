65143e6b972d0a3b746f5951a0d5a6ee2f6a87ae
Camilo Galeano <juan_camilo50@hotmail.com>
2020-09-27 17:14:35 +0200

    changed start script in angular-frontend package.json

--- server.js
+++ server.js
@@ -364,10 +364,10 @@ app.use('/', router);
 
 if(process.env.NODE_ENV === 'production'){
 
-    app.use(express.static('angular-frontend/dist'))
+    app.use(express.static('angular-frontend/dist/angular-csProject'))
 
     app.get('*', (req, res) => { 
-        res.sendFile(path.resolve(__dirname, 'angular-frontend', 'dist', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'angular-frontend', 'dist', 'angular-csProject', 'index.html'))
      })
 }
     
